2ed8ba1f29c9cb2c935d07654144a69e56e2aecf
Fendous <fendousteam@gmail.com>
2023-08-30 13:18:58 +0200

    initial commit for snowman melting game

---
+++ meltingSnowman.js
@@ -0,0 +1,14 @@
+const words = [
+  "adaptable",
+  "pocket",
+  "shock",
+  "crabby",
+  "stage",
+  "wealthy",
+  "birthday",
+  "grumpy",
+  "chance",
+  "produce",
+  "calendar",
+  "nonchalant",
+];
